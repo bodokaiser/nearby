801deccbb51539a6669714a0aed800d38c3efb74
bodokaiser <i@bodokaiser.io>
2013-10-08 18:49:03 +0200

    removed old changes

--- app/websocket.js
+++ app/websocket.js
@@ -2,21 +2,8 @@ module.exports = function(app) {
 
     var wsocket = new WebSocket(app.settings.websocket.url);
 
-    /**
-     * NOTE:
-     * The seems to be a bug on iPhone which causes the WebSocket connection
-     * to break up. To overcome this a solution is to decouple WebSocket events
-     * from GeoLocation events. However this makes stuff complicated so do not
-     * wonder...
-     */
-    var position;
-
     wsocket.addEventListener('open', function() {
-        if (!position) return;
-
-        var message = JSON.stringify(position);
- 
-        wssocket.send(message);
+        app.emit('websocket:open');
     });
 
     wsocket.addEventListener('message', function(e) {
@@ -25,20 +12,13 @@ module.exports = function(app) {
         app.emit('websocket:update', message);
     });
 
-    app.addListener('location:current', function(geometry) {
-        var message = JSON.stringify(geometry);
-        
-        if (wssocket.readyState === wssocket.OPEN) {
-            wsocket.send(message);
-        } else {
-            position = geometry;
-        }
-    });
+    app.addListener('location:current', pushGeometryToSocket);
+    app.addListener('location:update', pushGeometryToSocket);
 
-    app.addListener('location:update', function(geometry) {
+    function pushGeometryToSocket(geometry) {
         var message = JSON.stringify(geometry);
 
         wsocket.send(message);
-    });
+    }
 
 };
