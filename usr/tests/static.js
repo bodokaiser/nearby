55047d06beb319ed3613a73a58a2195cdeb49233
Bodo Kaiser <i@bodokaiser.io>
2018-10-06 17:19:20 +0200

    updated nearby for node v10 compatiblity

--- usr/tests/static.js
+++ usr/tests/static.js
@@ -1,15 +1,15 @@
-var supertest = require('supertest');
+const supertest = require('supertest')
 
-var app = require('../../lib');
+let app = require('../../lib')
 
 describe('static', function() {
 
-  it('should respond index.html', function(done) {
-    supertest(app).get('/').expect(200, done);
-  });
+  it('should respond index.html', done => {
+    supertest(app).get('/').expect(200, done)
+  })
 
-  it('should respond javascripts/build.js', function(done) {
-    supertest(app).get('/javascripts/build.js').expect(200, done);
-  });
+  it('should respond javascripts/build.js', done => {
+    supertest(app).get('/javascripts/build.js').expect(200, done)
+  })
 
-});
+})
